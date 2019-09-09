04b70df4c25c830bb9bb1d3fefe9dfe71df03e88
1nstinct <yura.syedin@gmail.com>
2019-09-09 15:00:44 +0300

    dotenv

--- src/common/main/config.js
+++ src/common/main/config.js
@@ -1,3 +1,3 @@
 export const API_URL = 'http://localhost:3001/fetch';
-export const GOOGLE_KEY = 'AIzaSyAPW4UhhJ8ciXSMOIrhy6SgnyO5nY_kyMk';
+export const GOOGLE_KEY = process.env.REACT_APP_GOOGLE_KEY;
 export const MIN_SEARCH_STR_LENGTH = 3;
